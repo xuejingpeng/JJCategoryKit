09d242160857dfc58a7c10aba5b186d4feafbe96
在乎科技 <icare@zaihukejideiMac.local>
2019-06-05 10:27:21 +0800

    JJCategoryKit

---
+++ Example/Pods/Target Support Files/JJCategoryKit/JJCategoryKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JJCategoryKit : NSObject
+@end
+@implementation PodsDummy_JJCategoryKit
+@end
---
+++ Example/Pods/Target Support Files/JJCategoryKit/JJCategoryKit-umbrella.h
@@ -0,0 +1,21 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "NSString+Addition.h"
+#import "PlaceholdTextView.h"
+#import "UIColor+ColorChange.h"
+#import "UIImage+UpdateImageSize.h"
+#import "UILabel+String.h"
+
+FOUNDATION_EXPORT double JJCategoryKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char JJCategoryKitVersionString[];
+
